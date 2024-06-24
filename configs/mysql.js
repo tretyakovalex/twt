0745c4a1fd05fbb03981d2d149c0d934db1ce602
tretyakovalex <tretyakovalexmax@gmail.com>
2024-06-24 13:14:01 +0200

    disable old db

--- configs/mysql.js
+++ configs/mysql.js
@@ -2,29 +2,29 @@ const mysql = require('mysql2');
 
 require('dotenv').config();
 
-const snPool = mysql.createPool({
-    host: '10.147.19.172',
-    port: '3306',
-    user: 'GSA',
-    password: '9379992a',
-    database: 'sn'
-});
+// const snPool = mysql.createPool({
+//     host: '10.147.19.172',
+//     port: '3306',
+//     user: 'GSA',
+//     password: '9379992a',
+//     database: 'sn'
+// });
 
-const taPool = mysql.createPool({
-    host: '10.147.19.172',
-    port: '3306',
-    user: 'GSA',
-    password: '9379992a',
-    database: 'ta'
-});
+// const taPool = mysql.createPool({
+//     host: '10.147.19.172',
+//     port: '3306',
+//     user: 'GSA',
+//     password: '9379992a',
+//     database: 'ta'
+// });
 
-const wo3Pool = mysql.createPool({
-    host: '10.147.19.172',
-    port: '3306',
-    user: 'GSA',
-    password: '9379992a',
-    database: 'wo3'
-});
+// const wo3Pool = mysql.createPool({
+//     host: '10.147.19.172',
+//     port: '3306',
+//     user: 'GSA',
+//     password: '9379992a',
+//     database: 'wo3'
+// });
 
 const twt = mysql.createPool({
     host: '127.0.0.1',
@@ -34,4 +34,5 @@ const twt = mysql.createPool({
     database: 'twt'
 });
 
-module.exports = { snPool, taPool, wo3Pool, twt};
+// module.exports = { snPool, taPool, wo3Pool, twt};
+module.exports = {twt};
