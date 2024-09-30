a9b63265f7d3eb14a8d42e4ec289527a5aa9344b
tretyakovalex <tretyakovalexmax@gmail.com>
2024-09-30 12:15:16 +0400

    added modifying results page, changed selection of sample number for purchases to use only if its result exists

--- routes/results/results-routes.js
+++ routes/results/results-routes.js
@@ -4,10 +4,14 @@ const router = express.Router();
 const { twt } = require('../../configs/mysql');
 
 router.get('/getResultsBySampleNumber', async (req, res) => {
-    const offer_number = req.query.offer_number;
+    const sample_number = req.query.sample_number;
     try {
-        twt.query('SELECT * FROM registrations WHERE offer_number = ?', offer_number, (err, result) => {
-            res.json({registration: result})
+        twt.query('SELECT * FROM results WHERE sample_number = ?', sample_number, (err, result) => {
+            if(err){
+                console.error(err);
+                res.status(500).json("Internal Server Error");
+            }
+            res.json({result: result})
         })
     } catch (error) {
         console.error(error);
@@ -20,11 +24,11 @@ router.post('/addResults', async (req, res) => {
         const columns = Object.keys(data);
         const values = Object.values(data);
 
-        console.log("Printing columns: ");
-        console.log(columns);
+        // console.log("Printing columns: ");
+        // console.log(columns);
 
-        console.log("Printing values: ");
-        console.log(values);
+        // console.log("Printing values: ");
+        // console.log(values);
 
         const query = `
             INSERT INTO results
@@ -33,12 +37,19 @@ router.post('/addResults', async (req, res) => {
             (${Array(columns.length).fill('?').join(', ')})
             `;
 
-        console.log(query);
+        // console.log(query);
 
         twt.query(query, values, (err, result) => {
             if (err) {
-                console.log(err);
-                return res.status(500).send('Internal Server Error');
+                if (err.code === 'ER_DUP_ENTRY') {
+                    return res.status(400).json({
+                        success: false,
+                        message: 'Duplicate entry: The sample_number already added to results.'
+                    });
+                } else {
+                    console.log(err);
+                    return res.status(500).send({message: 'Internal Server Error'});
+                }
             }
 
             res.json({ results: result });
@@ -48,6 +59,41 @@ router.post('/addResults', async (req, res) => {
         return res.status(500).send('Internal Server Error');
     }
 });
+
+router.put('/updateResults', async (req, res) => {
+    try {
+        const data = req.body;
+        console.log(data);
+
+        const query = `UPDATE results SET ? WHERE sample_number='${data.sample_number}';`;
+
+        twt.query(query, data, (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send({message: 'Internal Server Error'});
+            }
+
+            res.json({ results: result });
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Internal Server Error');
+    }
+});
+
+// === get registration sample numbers ===
+router.get('/getResultSampleNumbersByDate', async (req, res) => {
+    const date = req.query.date;
+    console.log(date);
+    try {
+        twt.query('SELECT DISTINCT sample_number FROM results WHERE date=?', [date], (err, result) => {
+            res.json({sampleNumbers: result})
+        })
+    } catch (error) {
+        console.error(error);
+    }
+})
+// =======================================
 
 
 module.exports = router;
